9f04c20e15fc1373e43bda84fd958187028247da
Subhankar Sahoo <subhankarsahoo003@gmail.com>
2023-02-18 11:12:12 +0530

    panel and auth added

---
+++ login.php
@@ -0,0 +1,91 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $user_type = $_POST['user_type'];
+
+   $select = " SELECT * FROM user_table WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+
+      if($row['user_type'] == 'backend'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         header('location:admin_panel.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         header('location:user_panel.php');
+
+      }elseif($row['user_type'] == 'police'){
+
+         $_SESSION['police_name'] = $row['name'];
+         header('location:police_panel.php');
+
+      }elseif($row['user_type'] == 'doctor'){
+
+         $_SESSION['doctor_name'] = $row['name'];
+         header('location:doctor_panel.php');
+
+      }elseif($row['user_type'] == 'hospital'){
+
+         $_SESSION['hospital_name'] = $row['name'];
+         header('location:hospital_panel.php');
+
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
